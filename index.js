46c342a0cd62d8ef48c7f799e2402c1b79ef04cc
SamirBenj <43613928+SamirBenj@users.noreply.github.com>
2021-04-27 15:00:25 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 8085;
+
+
+app.use(express.urlencoded());
+
+
+app.get('/', function (req, res) {
+    res.sendFile('/index.html', { root: __dirname });
+});
+
+app.post('/testAction', function (req, res) {
+    res.send('Welcome boyuyyy');
+});
+app.listen(PORT, function () {
+    console.log('server is running for IFAR !!');
+});
